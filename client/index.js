4d00b3901f6636f0ce5d0e09966e9e10aac22e50
Bryan Jennings <bryanjenningz@gmail.com>
2016-12-15 13:29:47 -0800

    change inline JSX to component

--- client/index.js
+++ client/index.js
@@ -1,5 +1,13 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {render} from 'react-dom'
 
+class App extends Component {
+  render() {
+    return (
+      <div>Hi</div>
+    )
+  }
+}
+
 const rootEl = document.querySelector('#root')
-render(<div>Hello World</div>, rootEl)
+render(<App />, rootEl)
